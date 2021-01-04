0db5200be993ecce1336e3648d8955cd1b82a7ca
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-04 15:52:46 +0100

    Add background image

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 "use strict";
 
 const menuBtn = document.querySelector(".js-menu-btn");
+const closeBtn = document.querySelector(".js-close-btn");
 const menuNav = document.querySelector(".js-nav");
 
 menuBtn.addEventListener("click", () => {
-  console.log("hola");
-  menuNav.classList.toggle("hidden");
+  menuNav.classList.remove("hidden");
+  menuBtn.classList.add("hidden");
+  // menuBtn.classList.toggle("hidden");
+});
+closeBtn.addEventListener("click", () => {
+  menuNav.classList.add("hidden");
+  menuBtn.classList.remove("hidden");
 });
